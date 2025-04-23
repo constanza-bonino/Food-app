a0a27745481610e14fc7a90fd15e935e652a8609
Felipe Alonso <alonso.felipe23@gmail.com>
2025-04-23 20:30:09 -0300

    inicial

---
+++ src/components/FoodProduct/FoodProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FoodProduct(props) {
+	return (
+		<div style={{width: "200px", height: "200px", border: "1px solid black"}}>
+			hola
+		</div>
+	);
+}
+
+export default FoodProduct;
---
+++ src/components/FoodTable/FoodTable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FoodProduct from '../FoodProduct/FoodProduct';
+
+function FoodTable(props) {
+	return (
+		<div>
+			<FoodProduct></FoodProduct>
+		</div>
+	);
+}
+
+export default FoodTable;
