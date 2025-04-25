bc9be1f9c51dc83a8ea0c34df08148c20af73c1e
Felipe Alonso <alonso.felipe23@gmail.com>
2025-04-24 22:56:28 -0300

    estilos de las cards de comidas y contenedor

--- src/components/FoodProduct/FoodProduct.jsx
+++ src/components/FoodProduct/FoodProduct.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import "./FoodProduct.css";
 function FoodProduct(props) {
+	const foodCountComponent = () => {
+		if (props.food.quantity > 0) {
+			return <span className="food_quantity">{props.food.quantity}</span>;
+		}
+	};
+
 	return (
 		<div className="food_item">
-			<p>{props.name}</p>
+			<img
+				className="food_thumbnail"
+				src={props.food.thumbnail_url}
+			></img>
+			<p>{props.food.name}</p>
+			<div className="food_product_info">
+				<span>${props.food.price}</span>
+				{foodCountComponent()}
+			</div>
 		</div>
 	);
 }
