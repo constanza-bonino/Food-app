973d8771afea4a8d82e94177fdca4b60e875e1a0
Felipe Alonso <alonso.felipe23@gmail.com>
2025-04-27 23:42:42 -0300

    title

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SideBar from "./components/SideBar/SideBar";
 function App() {
 	return (
 		<>
-			<h1 className="food-app-table">Food App</h1>
+			<h1 className="food-app-title">Food App</h1>
 			<div className="app-container">
 				<FoodTable></FoodTable>
 				<SideBar></SideBar>
